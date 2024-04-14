cd6c3cd89675e3080c53d4f6eb54f734941c9030
amekusa <hello@amekusa.com>
2024-04-15 00:44:44 +0900

    Breaking Change: use object keys for wildcard results

--- src/index.js
+++ src/index.js
@@ -58,16 +58,16 @@ function dig(obj, path, opts = {}) {
 		let iP = p[i];
 
 		if (iP == '*') { // wildcard
-			r.results = [];
+			r.results = {};
 			let keys = Object.keys(obj);
 			if (i == p.length - 1) {
 				// wildcard destination; add every property to results
-				for (let j = 0; j < keys.length; j++) r.results.push({found: obj[keys[j]]});
+				for (let j = 0; j < keys.length; j++) r.results[keys[j]] = {found: obj[keys[j]]};
 			} else {
 				// wildcard branching; dig every property one by one
 				let pRest = p.slice(i + 1);
 				for (let j = 0; j < keys.length; j++) {
-					if (isDiggable(obj[keys[j]])) r.results.push(dig(obj[keys[j]], pRest, opts)); // recursion
+					if (isDiggable(obj[keys[j]])) r.results[keys[j]] = dig(obj[keys[j]], pRest, opts); // recursion
 				}
 			}
 			return r;
